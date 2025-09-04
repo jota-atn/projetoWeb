a50db321307cea75050d4123bbcdc26dd85dd757
Jota <jamqueiroz2005@gmail.com>
2025-09-03 21:51:33 -0300

    Feature(Data): Cria dados de livros temporários, a fim de testes de estilização e debug

---
+++ js/data.js
@@ -0,0 +1,66 @@
+export const booksData = [
+    {
+        id: 1,
+        title: 'Deep Learning: Fundamentos e Aplicações',
+        author: 'Dr. Ana Silva',
+        price: 89.90,
+        coverImage: 'https://m.media-amazon.com/images/I/A10G+oKN3LL._UF894,1000_QL80_.jpg',
+        category: 'IA'
+    },
+    {
+        id: 2,
+        title: 'Python Avançado para Ciência de Dados',
+        author: 'Prof. Carlos Santos',
+        price: 79.90,
+        coverImage: 'https://m.media-amazon.com/images/I/91DGHmkQmjL._SL1500_.jpg',
+        category: 'Programação'
+    },
+    {
+        id: 3,
+        title: 'Ethical Hacking: Segurança Ofensiva',
+        author: 'Dra. Maria Oliveira',
+        price: 94.90,
+        coverImage: 'https://m.media-amazon.com/images/I/91LldWCllgS._SL1500_.jpg',
+        category: 'Cibersegurança'
+    },
+    {
+        id: 4,
+        title: 'Blockchain: Tecnologia e Aplicações',
+        author: 'Dr. João Pereira',
+        price: 84.90,
+        coverImage: 'https://m.media-amazon.com/images/I/61NqNm55lhL._SL1000_.jpg',
+        category: 'Cibersegurança'
+    },
+    {
+        id: 5,
+        title: 'Estruturas de Dados com JavaScript',
+        author: 'Fernanda Costa',
+        price: 68.50,
+        coverImage: 'https://m.media-amazon.com/images/I/71KGa1y8eaL.jpg',
+        category: 'Programação'
+    },
+    {
+        id: 6,
+        title: 'Inteligência Artificial: Uma Abordagem Moderna',
+        author: 'Russell & Norvig',
+        price: 159.90,
+        coverImage: 'https://m.media-amazon.com/images/I/81Sc7DUiVmL.jpg',
+        category: 'IA'
+    },
+    {
+        id: 7,
+        title: 'Redes de Computadores e a Internet',
+        author: 'Kurose & Ross',
+        price: 135.00,
+        coverImage: 'https://m.media-amazon.com/images/I/91huSJr6BML._UF894,1000_QL80_.jpg',
+        category: 'Redes'
+    },
+    {
+        id: 8,
+        title: 'Arquitetura Limpa: O Guia do Artesão',
+        author: 'Robert C. Martin',
+        price: 75.90,
+        coverImage: 'https://m.media-amazon.com/images/I/815d9tE7jSL.jpg',
+        category: 'Engenharia de Software'
+    }
+];
